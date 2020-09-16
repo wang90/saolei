3efba7617a6b9ce546474c377eab9e8446919533
wang90 <393210564@qq.com>
2020-09-16 18:13:52 +0800

    feat: 平铺算法逻辑,绑定视图

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,196 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <button @click="init">开始</button>
+      <div>剩余地雷数:{{count}}</div>
+    </div>
+    <div class="play-contain">
+      <div class="lines" 
+      v-for = "(line,index) in list" 
+      :key = index>
+        <div  
+          :key= i
+          v-for="(l,i) in line" 
+          @click="choose(l,index,i)">
+          <block 
+            :value="l.value"
+            :active="l.active"
+            :error="l.error"
+            :touched="l.touched"
+            ></block>
+        </div>
+      </div>
+    </div>
+      <h6>{{message}}</h6>
   </div>
 </template>
-
+<style>
+  /* .play-contain{
+    border:1px solid #ccc;
+  } */
+ .lines {
+    display: flex;
+  }
+</style>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const SAFE_CELL = 0;
+const MINE_CELL = 9;
+const ROWS  = 15;
+const COLS = 10;
+const AROUND = [
+  [ -1, -1 ],
+  [ -1, 0 ],
+  [ -1, 1 ],
+  [ 0, -1 ],
+  [ 0, 1 ] ,
+  [ 1, -1 ],
+  [ 1, 0 ],
+  [ 1, 1 ],
+]
+import Block from "@/components/block.vue";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      message:"",
+      list :[],
+      isPlay:true,
+      count: 10,
+      nums: 0,
+    }
+  },
+  components:{'block':Block},
+  created() {
+    this.init();
+  },
+  methods:{
+    init () {
+      this.isPlay = true;
+      this.message = "";
+      this.initData();
+    },
+    initData() {
+      const rows = ROWS;
+      const cols = COLS;
+      const mines = this.count;
+      const safeCellNum = rows * cols - mines;
+      const safeArea = (new Array(safeCellNum).fill(SAFE_CELL ));
+      const mineArea = (new Array(mines)).fill(MINE_CELL);
+      let totalArea = safeArea.concat(mineArea);
+      totalArea = this.minShuffle(totalArea);
+
+      const data = totalArea.reduce(( memo, curr, index ) => {
+        if (index % cols === 0 ) {
+          memo.push([curr])
+        }else {
+          memo[ memo.length - 1 ].push( curr )
+        }
+        return memo;
+      }, [] );
+      const list = this.setEnvNum(data);
+  
+      this.list = list.map(v=>{
+        return v.map(_v=>{
+          this.nums +=1;
+          return {
+            value: _v,
+            active: false,
+            error: false,
+            touched: false,
+          }
+        })
+      })
+    },
+    minShuffle (array, count = array.length) {
+      let index;
+      while(count) {
+        index = Math.floor(Math.random() * count -- );
+        [ array[ count ], array[ index ]] = [ array[ index ], array[ count ] ]
+      }
+      return array;
+    },
+    setEnvNum( list ) {
+      list.forEach(( row, rowIndex) => {
+          row.forEach(( cell, colIndex ) => {
+            if ( cell === MINE_CELL) {
+              AROUND.forEach( offset => {
+                const row = rowIndex + offset[0];
+                const col = colIndex + offset[1];
+                if ( list[row] &&
+                  list[row][col] !== undefined && 
+                  list[row][col] !== MINE_CELL ) { 
+                    list[row][col] ++ ;
+                }
+              })
+            }
+          })
+      });
+      return list;
+    },
+    playEnd(){
+      this.message = '你踩到地雷了';
+      this.isPlay = false;
+      this.list.map(v => {
+        return v.map( _v => {
+          if ( _v.active === false) {
+            _v.active = true
+          }
+          return v;
+        })
+      })
+    },
+    choose( e, row, col ) {
+      if ( this.isPlay && !e.active) {
+        if ( e.value === 9) {
+          this.$set( this.list, this.list[row][col]['error'] = true);
+          this.$set( this.list, this.list[row][col]['touched'] = true);
+          this.$set( this.list, this.list[row][col]['active'] = true );
+          this.playEnd();
+        } else if ( e.value === 0 ) {
+          console.log('需要遍历');
+          this.searchValueNone({ row, col } )
+        } else {
+          this.$set( this.list, this.list[row][col]['active'] = true );
+          this.$set( this.list, this.list[row][col]['touched'] = true );
+        }
+      }
+    },
+    searchValueNone ( data ) {
+      const  { row , col } = data;
+      this.searchValue( row , col);
+    },
+    searchValue(rowIndex, colIndex) {
+
+      const currentIndex = this.list[rowIndex] ? this.list[rowIndex][colIndex] || null : null;
+      if (currentIndex &&  currentIndex['active'] === false) {
+        if ( currentIndex['value'] === 0 ) {
+          this.$set( this.list, this.list[rowIndex][colIndex]['active'] = true );
+          AROUND.forEach( offset => {
+            const row = rowIndex + offset[0];
+            const col = colIndex + offset[1];
+            const current = this.list[row]? this.list[row][col] || null : null;
+            if ( current ) {
+              if ( current['active'] === false && current['value'] !== 9 ) {
+                
+                if ( current['value'] !== 9 ) {
+                  if (current['value'] !== 0 ) {
+                    this.$set( this.list, this.list[row][col]['active'] = true );
+                  } else {
+                    this.searchValue( row, col );
+                  }
+                } else {
+                  this.$set( this.list, this.list[row][col]['active'] = true );
+                }
+              }
+            } 
+          })
+        } else if (currentIndex['value'] !== 9) {
+          // this.$set( this.list, this.list[rowIndex][colIndex]['active'] = true );
+        }
+      }
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/block.vue
@@ -0,0 +1,92 @@
+<template>
+    <div :class="[
+        'block',
+        {'active':active},
+        {'touched':touched},
+        {'error':error},
+        {'none': active && value === 0},
+        {'die': active && value === 9}
+    ]">{{getValue}}</div>
+</template>
+<style>
+  .block {
+    width: 25px;
+    border:1px solid #ccc;
+    height: 25px;
+    text-align: center;
+    line-height: 25px;
+    position: relative;
+    cursor: pointer;
+  }
+  .block::before  {
+    content: "";
+    position: absolute;
+    left: 1px;
+    top: 1px;
+    right: 1px;
+    bottom: 1px;
+    background: #fff;
+  }
+  .block.touched{
+    background: rgba(0, 128, 0, 0.2);
+  }
+  .block.error{
+    background: rgba(255, 0, 0, 0.2);
+  }
+  .block.active{
+    background: rgba(173, 255, 47, 0.2);
+  }
+  .block.none{
+    background:#eee;
+  }
+  .block.die{
+    background: url('../assets/die.png') no-repeat  center ;
+    background-size: 70%;
+  }
+  .block.touched.die{
+    background-color: rgba(255, 0, 0, 0.2);
+  }
+  .block.active::before{
+    display: none;
+  } 
+</style>
+<script>
+
+export default {
+    name: 'Block',
+    computed:{
+        getValue() {
+            if (this.value === 0 || this.value === 9 ) {
+                return "";
+            } 
+            return this.value;
+        }
+    },
+    props:{
+        value: {
+            type: Number,
+            default: 0 ,
+            required:true,
+        },
+        active: {
+            type: Boolean,
+            default: false ,
+            required:true,
+        },
+        error: {
+            type: Boolean,
+            default: false ,
+            required:true,
+        },
+        touched: {
+            type: Boolean,
+            default: false,
+            required:true,
+        }
+    },
+    data() {
+        return {}
+    },
+}
+</script>
+
